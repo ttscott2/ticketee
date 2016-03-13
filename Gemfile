850dc4e483c533dd6f68ca89d3ce3a28b4a6ba40
Tim Scott <timothytscott3@gmail.com>
2016-03-13 09:03:43 +0000

    Add Devise + sign up feature

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'font-awesome-rails', '~> 4.5', '>= 4.5.0.1'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
+gem 'devise', '~> 3.5', '>= 3.5.6'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313073718) do
+ActiveRecord::Schema.define(version: 20160313085027) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -41,8 +41,8 @@ ActiveRecord::Schema.define(version: 20160313073718) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
