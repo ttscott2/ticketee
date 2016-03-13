0a75561d97d4773f4f773b049fe21a1049fd92a2
Tim Scott <timothytscott3@gmail.com>
2016-03-13 08:08:50 +0000

    Implement creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
     root "projects#index"
-    resources :projects
+    resources :projects do
+        resources :tickets
+    end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +58,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
