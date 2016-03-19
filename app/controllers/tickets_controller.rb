1bb17aa1f877d59d962777dfbcc09b6d6762fd7d
Tim Scott <timothytscott3@gmail.com>
2016-03-19 05:52:55 +0000

    Only managers and admins can delete tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -42,6 +42,7 @@ class TicketsController < ApplicationController
     end
     
     def destroy
+      authorize @ticket, :destroy?
       @ticket.destroy
       flash[:notice] = "Ticket has been deleted."
       
--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -38,6 +38,11 @@ RSpec.feature "Users can only see the appropriate links" do
       visit project_path(project)
       expect(page).not_to have_link "New Ticket"
     end
+    
+    scenario "cannot see the Delete Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link "Delete Ticket"
+    end
   end
 
   context "admin users" do
@@ -63,6 +68,11 @@ RSpec.feature "Users can only see the appropriate links" do
     scenario "can see the Edit Ticket link" do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link "Edit Ticket"
+    end
+    
+    scenario "can see the Delete Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link "Delete Ticket"
     end
   end
 end
